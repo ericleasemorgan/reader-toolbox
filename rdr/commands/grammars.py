04883d4d7efab7c5079fc291efc2942382129462
Eric Lease Morgan <eric_morgan@infomotions.com>
2021-08-08 18:19:07 -0400

    Trying to add -h options

--- rdr/commands/grammars.py
+++ rdr/commands/grammars.py
@@ -4,116 +4,96 @@
 # require
 from rdr import *
 
-# features
+
+# filter
+def applyFilter ( filter, features ) :
+
+	# initialize and process each feature
+	filtered = []
+	for feature in features :
+				
+		# branch according to type; Span
+		if str( type( feature ) ) == "<class 'spacy.tokens.span.Span'>" : 
+		
+			# update, conditionally
+			if filter in feature.text : filtered.append( feature )
+
+		# hope for a tuple with three elements
+		else :
+		
+			# update, conditionally
+			if ( ( filter in feature[ 0 ].text ) or
+			     ( filter in feature[ 1 ].text ) or 
+			     ( filter in feature[ 2 ].text ) ) : filtered.append( feature )
+		
+	# done
+	return ( filtered )
+
+
+# output
+def outputFeatures( features ) :
+
+	# process each feature
+	for feature in features :
+			
+		# branch accordingly
+		if str( type( feature ) ) == "<class 'spacy.tokens.span.Span'>" : click.echo( feature.text )
+		else : click.echo( "\t".join( ( feature[ 0 ].text, feature[ 1 ].text, feature[ 2 ].text ) ) )
+
+
+# grammars
 @click.command( options_metavar='<options>' )
-@click.option('-e', '--entity', type=click.STRING, help="only applicable to grammar sss; a noun or noun phrase")
-@click.option('-v', '--verb', default='be', help="only applicable to grammar sss; the lemma of a verb, such as 'be', 'have', or 'say'")
+@click.option('-f', '--filter', type=click.STRING, help="limit results to include the given word")
+@click.option('-n', '--noun', type=click.STRING, help="only applicable to sss; a noun or noun phrase")
+@click.option('-v', '--verb', default='be', help="only applicable to sss; the lemma of a verb, such as 'be', 'have', or 'say'")
 @click.argument( 'carrel', metavar='<carrel>' )
-@click.argument( 'grammar', type=click.Choice( [ 'svo', 'sss', 'nouns', 'quotes' ], case_sensitive=False ) )
-def grammars( carrel, grammar, entity, verb ) :
+@click.argument( 'grammar', type=click.Choice( [ 'svo', 'sss', 'nouns', 'quotes' ], case_sensitive=True ) )
+def grammars( carrel, grammar, filter, noun, verb ) :
 
 	"""Extract grammatical sentence fragments from <carrel> where fragments are one of:
 	
-	  \b
+	\b
 	  * nouns - all the nouns and noun chunks
 	  * quotes - things people say
 	  * svo - fragments in the form of subject-verb-object
-	  * sss - fragments beginning with an entity, are connected with a
-	    verb, and are followed by a phrase; a more advanced version of
-	    svo
-
-	The use of language requires patterns and rules. We call theses things grammars. Unlike the tabulation of ngrams, parts-of-speech, named-entities, etc., the use of grammars enables one to extract complete thoughts. The results of theses functions are tab-delimited lists, and therefore they are amenable to post-processing with utilities such as sort, grep, and less. Use 'rdr pos' to identify interesting values for the sss grammar.
+	  * sss - a more advanced version of svo; fragments beginning
+	    with an entity, are connected with a verb, and are followed
+	    by a phrase
 	
 	Examples:
 	
 	\b
 	  * rdr grammars homer svo
-	  * rdr grammars homer svo | sort | less
-	  * rdr grammars homer svo | sort | less -x18
-	  * rdr grammars -e hector homer sss 
-	  * rdr grammars -e she -v have homer sss 
-	  * rdr grammars homer quotes
-	  * rdr grammars homer nouns
-	  * rdr grammars homer nouns | sort | uniq -c | sort -rn | less
+	  * rdr grammars -f horse homer svo
 	"""
 
 	# require
 	from textacy import extract
 	from os      import system
-	
-	# subjects-verbs-objects
-	if grammar == 'svo' :
-	
-		# initialize and get the features
-		doc      = carrel2doc( carrel )
-		features = list( extract.subject_verb_object_triples( doc ) )
-	
-		# process each one
-		for feature in features : 
 		
-			# parse and output
-			subject = feature[ 0 ].text
-			verb    = feature[ 1 ].text
-			object  = feature[ 2 ].text
+	# initialize
+	doc = carrel2doc( carrel )
 
-			# output
-			click.echo( "\t".join( ( subject, verb, object ) ) )
-	
-	# noun chunks
-	elif grammar == 'nouns' :
-
-		# initialize
-		doc      = carrel2doc( carrel )
-		features = list( extract.noun_chunks( doc ) )
-
-		# process each one
-		for feature in features : 
-		
-			# parse and output
-			chunk = feature.text
-
-			# output
-			click.echo( chunk )
-	
-	# semistructured statements
-	elif grammar == 'sss' :
+	# get the features
+	if   grammar == 'svo'    : features = list( extract.subject_verb_object_triples( doc ) )
+	elif grammar == 'quotes' : features = list( extract.direct_quotations( doc ) )
+	elif grammar == 'nouns'  : features = list( extract.noun_chunks( doc ) )
+	elif grammar == 'sss'    :
 
 		# sanity check
-		if not entity :
+		if not noun :
 		
-			click.echo( "Error: When specifyig type sss, the -e option is required.", err=True )
+			click.echo( "Error: When specifying grammar sss, the -n option is required.", err=True )
 			system( 'rdr grammars --help' )
 			exit()
 			
-		# initialize
-		doc      = carrel2doc( carrel )
-		features = list( extract.semistructured_statements( doc, entity=entity, cue=verb ) )
+		# do the work
+		features = list( extract.semistructured_statements( doc, entity=noun, cue=verb ) )
 
-		# process each one
-		for feature in features : 
-		
-			# parse and output
-			entity   = feature[ 0 ].text
-			cue      = feature[ 1 ].text
-			fragment = feature[ 2 ].text
-
-			# output
-			click.echo( "\t".join( ( entity, cue, fragment ) ) )
+	# filter, conditionally
+	if filter : features = applyFilter( filter, features )
 	
-	# direct quotes
-	elif grammar == 'quotations' :
-
-		# initialize
-		doc      = carrel2doc( carrel )
-		features = list( extract.direct_quotations( doc ) )
+	# output
+	outputFeatures( features )
 	
-		# process each one
-		for feature in features : 
-		
-			# parse and output
-			speaker   = feature[ 0 ].text
-			verb      = feature[ 1 ].text
-			quotation = feature[ 2 ].text
 
-			# output
-			click.echo( "\t".join( ( speaker, verb, quotation ) ) )
--- rdr/commands/ngrams.py
+++ rdr/commands/ngrams.py
@@ -6,11 +6,13 @@ from rdr import *
 
 # initialize
 @click.command( options_metavar='<options>' )
+@click.option('-h', '--human', is_flag=True, help='output in a more human-readable form')
+@click.option('-f', '--filter', type=click.STRING, help="limit results to include the given word")
 @click.argument( 'carrel', metavar='<carrel>' )
 @click.argument( 'n', metavar='<n>' )
 
 # ngrams
-def ngrams( carrel, n ) :
+def ngrams( carrel, n, filter, human ) :
 
 	"""Given a <carrel>, output ngrams of size <n>"""
 
@@ -30,6 +32,19 @@ def ngrams( carrel, n ) :
 	# create the set of ngrams
 	ngrams = list( nltk.ngrams( tokens, n ) )
 	
+	# filter, conditionally
+	if filter :
+	
+		# initialize and process each ngram
+		filtered = []
+		for ngram in ngrams :
+				
+			# check and update
+			if filter in ngram : filtered.append( ngram )
+		
+		# done
+		ngrams = filtered
+
 	# remove stopwords from unigrams or bigrams
 	if n < 3 :
 	
@@ -54,5 +69,27 @@ def ngrams( carrel, n ) :
 		# done; make it read pretty
 		ngrams = results
 		
-	# output; done
-	for ngram in ngrams : click.echo( "\t".join( list( ngram ) ) )
+	# output; human
+	if human :
+	
+		# initialize a dictionary and process each ngram
+		items = {}
+		for ngram in ngrams :
+
+			# update the dictionary
+			if ngram in items : items[ ngram ] += 1
+			else              : items[ ngram ]  = 1
+
+		# sort the dictionary; return the ngrams
+		ngrams = sorted( items.items(), key=lambda x:x[ 1 ], reverse=True )
+		
+		# process each ngram
+		for ngram in ngrams :
+			
+			# create a record and output
+			record = str( ngram[ 1 ] ) + '\t' + '\t'.join( list( ngram[ 0 ] ) )
+			click.echo( record )
+		
+	# output raw data, and hope sort, uniq, grep, and less are used
+	else :
+		for ngram in ngrams : click.echo( "\t".join( list( ngram ) ) )
