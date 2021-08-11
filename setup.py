06e70de914b0441432cfde3c15b11b70c6a9d5f0
Eric Lease Morgan <eric_morgan@infomotions.com>
2021-08-11 10:30:26 -0400

    Enhancing installation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[ 'Click' ],
+    install_requires=[ 'Click', 'requests', 'scipy', 'sklearn', 'matplotlib', 'nltk', 'textacy' ],
     entry_points={ 'console_scripts': [ 'rdr = rdr.rdr:rdr' ],
     }
 )
