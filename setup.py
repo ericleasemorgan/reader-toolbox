c9b86d641e6af92409be64a67cb7c5c9f04a1a40
Eric Lease Morgan <eric_morgan@infomotions.com>
2021-08-12 12:18:23 -0400

    Adding dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[ 'Click', 'requests', 'scipy', 'sklearn', 'matplotlib', 'nltk', 'textacy' ],
+    install_requires=[ 'click<7.2.0,>=7.1.1', 'requests', 'scipy', 'sklearn', 'matplotlib', 'nltk', 'textacy', 'gensim<=3.8.3', 'pyLDAvis' ],
     entry_points={ 'console_scripts': [ 'rdr = rdr.rdr:rdr' ],
     }
 )
