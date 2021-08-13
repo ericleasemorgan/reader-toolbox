13b8a36416937c12e0d41bbff87c353d21aa527b
Eric Lease Morgan <eric_morgan@infomotions.com>
2021-08-12 21:48:10 -0400

    Added specific versions of packages

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[ 'click<7.2.0,>=7.1.1', 'requests', 'scipy', 'sklearn', 'matplotlib', 'nltk', 'textacy', 'gensim<=3.8.3', 'pyLDAvis' ],
+    install_requires=[ 'click==7.1.2', 'requests==2.26.0', 'scipy==1.7.1', 'scikit-learn==0.24.2', 'matplotlib==3.4.2', 'nltk==3.6.2', 'textacy==0.11.0', 'gensim<=3.8.3', 'pyLDAvis' ],
     entry_points={ 'console_scripts': [ 'rdr = rdr.rdr:rdr' ] }
 )
