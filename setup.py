49cdf004faa36a0eb994d3b3335d18605c1ea210
Eric Lease Morgan <eric_morgan@infomotions.com>
2021-08-14 08:58:32 -0400

    Learning to to distribute

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rdr',
-    version='0.1.0',
+    name='reader-toolbox',
+    version='0.1.01',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[ 'click==7.1.2', 'datasette', 'requests==2.26.0', 'scipy==1.7.1', 'scikit-learn==0.24.2', 'matplotlib==3.4.2', 'nltk==3.6.2', 'textacy==0.11.0', 'gensim<=3.8.3', 'pyLDAvis' ],
+    install_requires=[ 'click<7.2.0,>=7.1.1', 'datasette', 'requests', 'scipy', 'scikit-learn', 'matplotlib', 'nltk', 'textacy', 'gensim<=3.8.3', 'pyLDAvis' ],
     entry_points={ 'console_scripts': [ 'rdr = rdr.rdr:rdr' ] }
 )
