730367b6edcb476d551dc7f127876302ec81b70e
Eric Lease Morgan <eric_morgan@infomotions.com>
2021-09-10 09:03:25 -0400

    Tweaked requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='reader-toolbox',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[ 'click<7.2.0,>=7.1.1', 'datasette', 'requests', 'scipy', 'scikit-learn', 'matplotlib', 'nltk', 'textacy', 'gensim<=3.8.3', 'pyLDAvis' ],
+    install_requires=[ 'click<7.2.0,>=7.1.1', 'datasette', 'requests', 'scipy', 'scikit-learn', 'matplotlib', 'nltk', 'textacy', 'gensim<=3.8.3', 'pyLDAvis', 'word2vec' ],
     entry_points={ 'console_scripts': [ 'rdr = rdr.rdr:rdr' ] }
 )
