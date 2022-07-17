17edd80695f4e6076468fa59fa8f4e6763bb0ff5
Eric Lease Morgan <eric_morgan@infomotions.com>
2022-07-16 21:53:06 -0400

    Adding additional features

--- rdr/server.py
+++ rdr/server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-# reader-server.py - an HTTP front-end to the Reader Toolbox
+# server.py - an HTTP front-end to the Reader Toolbox
 
 # Eric Lease Morgan <emorgan@nd.edu>
 # (c) University of Notre Dame; distributed under a GNU Public License
 
 # July 14, 2022 - first cut; "Thanks Don!"
+# July 15, 2022 - adding many routes
+
 
 # configure
 CARREL = 'homer'
@@ -22,9 +24,11 @@ import rdr
 # initialize
 server = Flask( __name__ )
 
+
 # home page
 @server.route( '/' )
 def home() : return render_template( 'index.htm', error=None )
+
 
 # catalog
 @server.route( '/catalog' )
@@ -33,7 +37,8 @@ def catalog() :
 	# do the work and return it
 	result = rdr.catalog()
 	return( '<pre>%s</pre>' % result )
-	
+
+
 # concordance
 @server.route( '/concordance' )
 def concordance() :
@@ -50,7 +55,8 @@ def concordance() :
 
 	# do the work and return it
 	result = rdr.concordance( carrel, query=query, width=int( width ) )
-	return render_template( 'concordance.htm', result=result, carrel=carrel )
+	return render_template( 'concordance.htm', result=result, carrel=carrel, query=query )
+
 
 # ngrams
 @server.route( '/ngrams' )
@@ -79,15 +85,78 @@ def browse( carrel ) :
 	localLibrary = str( rdr.configuration( 'localLibrary' ) )
 
 	# Joining the base and the requested path
-	abs_path = os.path.join( localLibrary, carrel )
+	path = os.path.join( localLibrary, carrel )
 
 	# Return 404 if path doesn't exist
-	if not os.path.exists( abs_path ) : return abort( 404 )
+	if not os.path.exists( path ) : return abort( 404 )
 
 	# Check if path is a file and serve
-	if os.path.isfile( abs_path ) : return send_file( abs_path )
+	if os.path.isfile( path ) : return send_file( path )
 
 	# Show directory contents
-	files = os.listdir( abs_path )
+	files = os.listdir( path )
 	files.sort()
 	return render_template( 'files.htm', files=files )
+
+
+# full text search
+@server.route( '/search' )
+def search() :
+
+	# configure
+	carrel = CARREL
+	query  = 'love'
+	output = 'human'
+
+	# get input
+	carrel = request.args.get( 'carrel', carrel )
+	query  = request.args.get( 'query', query )
+	output = request.args.get( 'output', output )
+
+	# do the work and return it
+	result = rdr.search( carrel, query=query, output=output )
+	return render_template( 'search.htm', result=result, carrel=carrel, query=query )
+
+
+# info
+@server.route( '/info' )
+def info() :
+
+	# configure
+	carrel = CARREL
+
+	# get input
+	carrel = request.args.get( 'carrel', carrel )
+
+	# do the work
+	process     = rdr.provenance( carrel, 'process' )
+	originalID  = rdr.provenance( carrel, 'originalID' )
+	dateCreated = rdr.provenance( carrel, 'dateCreated' )
+	timeCreated = rdr.provenance( carrel, 'timeCreated' )
+	creator     = rdr.provenance( carrel, 'creator' )
+	input       = rdr.provenance( carrel, 'input' )
+	items       = rdr.extents( carrel, 'items' )
+	words       = rdr.extents( carrel, 'words' )
+	flesch      = rdr.extents( carrel, 'flesch' )
+	
+	# return it
+	return render_template( 'info.htm', carrel=carrel, process=process, originalID=originalID, dateCreated=dateCreated, timeCreated=timeCreated, creator=creator, input=input, items=items, words=words, flesch=flesch )
+
+
+# keywords
+@server.route( '/wrd' )
+def wrd() :
+
+	# configure
+	carrel = CARREL
+	cnt    = False
+
+	# get input
+	carrel = request.args.get( 'carrel', carrel )
+	cnt    = request.args.get( 'cnt', cnt )
+
+	# do the work and return it
+	result = rdr.keywords( carrel, count=cnt )
+	return render_template( 'keywords.htm', carrel=carrel, cnt=cnt, result=result )
+
+
