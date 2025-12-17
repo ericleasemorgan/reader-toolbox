4f50bdf5944624748b6e0ae9283b0232501d67bc
Eric Lease Morgan <eric_morgan@infomotions.com>
2025-12-17 13:19:51 -0500

    Change graph output to digraph

--- rdr/__init__.py
+++ rdr/__init__.py
@@ -720,7 +720,7 @@ def graph2gml( carrel, output='gml', save=False, erase=False, localLibrary=None 
 		edges.append( ( subject, object, { 'types' : 'creator' } ) )
 
 	# build the graph; very smart
-	graph = networkx.Graph()
+	graph = networkx.DiGraph()
 	graph.add_nodes_from( nodes )
 	graph.add_edges_from( edges )
 
